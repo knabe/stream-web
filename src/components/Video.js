98ad2654ce614722c0bb71c8aba97ce19ee35a98
Budimir <budimirbudimir@msn.com>
2017-10-07 16:28:51 +0200

    Added nicer player and fixed video styles

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { DefaultPlayer as Movie } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 import video from '../images/DKVideo.mp4';
 
 import './Video.css';
+
+// <video controls>
+//     <source src={video} type="video/mp4"/>
+//     Your browser is not supported. Please use latest version of Chrome or Firefox and try again.
+// </video>
 
 class Video extends Component {
   render() {
@@ -10,10 +17,9 @@ class Video extends Component {
 
     return (
       <section className={'Video col-xs-12' + width_class}>
-        <video controls>
-            <source src={video} type="video/mp4"/>
-            Your browser is not supported. Please use latest version of Chrome or Firefox and try again.
-        </video>
+        <Movie controls={['PlayPause', 'Seek', 'Volume', 'Fullscreen']}>
+            <source src={video} type="video/mp4" />
+        </Movie>
       </section>
     );
   }
