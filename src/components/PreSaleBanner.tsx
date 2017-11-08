5c7d75303f98f0a0014ab98aabfc9eda79308c2e
Simar Mangat <smangat@stanford.edu>
2017-11-08 10:20:49 -0800

    added fb pixel

--- src/components/PreSaleBanner.tsx
+++ src/components/PreSaleBanner.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
+import * as ReactPixel from 'react-facebook-pixel'
 // import { Link } from 'react-router-dom';
 
 import './PreSaleBanner.css'
 
-public trackPresaleInterest() {
-  ReactPixel.track('Lead', {content_name: 'PresaleInterest'})
-}
 
 class PreSaleBanner extends React.Component {
+
+  public trackPresaleInterest() {
+    ReactPixel.track('Lead', {content_name: 'PresaleInterest'})
+  }
+
   public render() {
     return (
-      <div classNam e="PreSaleBanner">
+      <div className="PreSaleBanner">
         <span>PRESALE OPEN NOW</span>
         <a className="btn" href="https://streamtoken.typeform.com/to/QlQ6Gp" onClick={this.trackPresaleInterest} >EXPRESS INTEREST</a>
       </div>
